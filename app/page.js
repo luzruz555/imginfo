7beebeae6a3375aead4108476a8bc10a3cc56a80
luzruz555 <seongirum555@gmail.com>
2025-12-05 10:32:38 +0900

    Add Home component with status image API instructions

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', maxWidth: '800px', margin: '0 auto' }}>
+      <h1>Status Image API</h1>
+      <p>사용법: /api/status?location=위치&date=날짜&time=시간&job=직업&faction=소속&char=캐릭터&emoji=이모지&relation=관계&incident=사건</p>
+    </div>
+  )
+  }
